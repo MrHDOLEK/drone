33361a754866efb660e5f14f107e2d6f365b00fa
Aleksander Kowalski <mrhdolek14@gmail.com>
2024-03-24 21:07:28 +0100

    fix: refactor

--- php/index.php
+++ php/index.php
@@ -4,52 +4,7 @@ declare(strict_types=1);
 
 require "./vendor/autoload.php";
 
-use Drone\Game\GameBuilder;
-use Drone\Game\GameEngine;
-use Drone\Game\Rules\GameRules;
-use Drone\Game\Rules\ScoreLimitRule;
-use Drone\Game\Rules\TimeLimitRule;
-use Drone\Scoreboard;
-use Drone\Team\TeamCollection;
-use League\CLImate\CLImate;
+use Drone\Tournament;
 
-$cli = new CLImate();
-$teams = GameBuilder::buildFromYaml("./resources/teams.yaml");
-$gameEngine = new GameEngine();
-$scoreboard = new Scoreboard();
-$gameRules = new GameRules(
-    new ScoreLimitRule(),
-    new TimeLimitRule(),
-);
-
-while ($teams->count() > 1) {
-    $games = $gameEngine->createGames($teams);
-
-    $nextRoundTeams = [];
-    foreach ($games->items() as $game) {
-        while (true) {
-            $cli->yellow()->info("Starting game: {$game->homeTeam()->name()} vs {$game->awayTeam()->name()}");
-
-            $gameEngine->simulateGame($game);
-
-            $cli->green()->info("Updating score: Home - {$game->score()->homeScore()}, Away - {$game->score()->awayScore()}");
-
-            if (!$gameRules->isSatisfiedBy($game)) {
-                $cli->red()->info("Finishing game: {$game->homeTeam()->name()} vs {$game->awayTeam()->name()}");
-                $nextRoundTeams[] = $game->winner();
-                $scoreboard->add($game);
-                break;
-            }
-        }
-    }
-
-    $teams = new TeamCollection(...$nextRoundTeams);
-}
-
-$cli->lightBlue()->info("Tournament Summary:");
-foreach ($scoreboard->items() as $summaryGame) {
-    $cli->cyan()->info("{$summaryGame->homeTeam()->name()} vs {$summaryGame->awayTeam()->name()} - Score: {$summaryGame->score()->homeScore()}-{$summaryGame->score()->awayScore()}");
-}
-
-$champion = $teams->first();
-$cli->magenta()->info("Champion: {$champion->name()}");
+$tournament = new Tournament("./resources/teams.yaml");
+$tournament->run();
---
+++ php/src/Tournament.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drone;
+
+use Drone\Game\Game;
+use Drone\Game\GameBuilder;
+use Drone\Game\GameEngine;
+use Drone\Game\Rules\GameRules;
+use Drone\Game\Rules\ScoreLimitRule;
+use Drone\Game\Rules\TimeLimitRule;
+use Drone\Team\TeamCollection;
+use Exception;
+use League\CLImate\CLImate;
+
+class Tournament
+{
+    private CLImate $cli;
+    private GameEngine $gameEngine;
+    private Scoreboard $scoreboard;
+    private GameRules $gameRules;
+    private TeamCollection $teams;
+
+    public function __construct(string $teamFilePath)
+    {
+        $this->cli = new CLImate();
+        $this->gameEngine = new GameEngine();
+        $this->scoreboard = new Scoreboard();
+        $this->gameRules = new GameRules(
+            new ScoreLimitRule(),
+            new TimeLimitRule(),
+        );
+        $this->teams = GameBuilder::buildFromYaml($teamFilePath);
+    }
+
+    public function run(): void
+    {
+        while ($this->teams->count() > 1) {
+            $this->playRound();
+        }
+        $this->displayTournamentSummary();
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function playRound(): void
+    {
+        $games = $this->gameEngine->createGames($this->teams);
+        $nextRoundTeams = [];
+
+        foreach ($games->items() as $game) {
+            $this->playGame($game);
+            $nextRoundTeams[] = $game->winner();
+        }
+
+        $this->teams = new TeamCollection(...$nextRoundTeams);
+    }
+
+    private function playGame(Game $game): void
+    {
+        while (true) {
+            $this->cli->yellow()->info("Starting game: {$game->homeTeam()->name()} vs {$game->awayTeam()->name()}");
+            $this->gameEngine->simulateGame($game);
+            $this->cli->green()->info("Updating score: Home - {$game->score()->homeScore()}, Away - {$game->score()->awayScore()}");
+
+            if (!$this->gameRules->isSatisfiedBy($game)) {
+                $this->cli->red()->info("Finishing game: {$game->homeTeam()->name()} vs {$game->awayTeam()->name()}");
+                $this->scoreboard->add($game);
+                break;
+            }
+        }
+    }
+
+    private function displayTournamentSummary(): void
+    {
+        $this->cli->lightBlue()->info("Tournament Summary:");
+        foreach ($this->scoreboard->items() as $summaryGame) {
+            $this->cli->cyan()->info("{$summaryGame->homeTeam()->name()} vs {$summaryGame->awayTeam()->name()} - Score: {$summaryGame->score()->homeScore()}-{$summaryGame->score()->awayScore()}");
+        }
+
+        $champion = $this->teams->first();
+        $this->cli->magenta()->info("Champion: {$champion->name()}");
+    }
+}
